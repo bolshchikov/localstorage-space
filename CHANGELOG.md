eea640425a30b89a74876cff4f11571c237ce128
Sergey Bolshchikov <sergey@bolshchikov.net>
2014-12-09 21:24:08 +0200

    chore: release v1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,10 @@
+<a name="1.0.0"></a>
+## 1.0.0 (2014-12-09)
+
+
+#### Features
+
+* implement space and spaceRemained ([10cf4cc2](https://github.com/bolshchikov/localstorage-space/commit/10cf4cc2d971041331b47367a87d41ed304acfa1))
+
+
 
---
+++ localstorage-space.js
@@ -0,0 +1,67 @@
+/*!
+ * localstorage-space - v1.0.0 - 2014-12-09
+ * https://github.com/bolshchikov/localstorage-space
+ *
+ * Copyright (c) 2014 Sergey N. Bolshchikov
+ * Licensed MIT
+ */
+
+'use strict';
+(function (window) {
+var KEY = 'bolshchikov';
+  var fill = new Array(1025 * 5).join('x');
+
+  var ls = window.localStorage;
+  var counter = window.bytesCounter.count;
+  var curr = fill;
+
+  var availableSpace = 0;
+
+  var setFill = function () {
+    try {
+      var record = ls.getItem(KEY);
+      var value = (record === null) ? '' : record;
+      value += curr;
+      ls.setItem(KEY, value);
+      setFill();
+    }
+    catch(err) {
+      if (curr.length !== 1) {
+        curr = curr.slice(curr.length / 2);
+        setFill();
+      }
+    }
+  };
+
+  var calculateSize = function () {
+    var i = 0, keys = Object.keys(ls), len = keys.length, acc = 0;
+    for (i; i < len; i += 1) {
+      acc = acc + counter(keys[i]) + counter(ls.getItem(keys[i]));
+    }
+    return acc;
+  };
+
+  var clearFill = function () {
+    ls.removeItem(KEY);
+  };
+
+  var calculateAvailableSpace = function () {
+    if (!availableSpace) {
+      setFill();
+      availableSpace = calculateSize();
+      clearFill();
+    }
+    return availableSpace;
+  };
+
+  Object.defineProperty(ls, 'space', {
+    get: calculateAvailableSpace
+  });
+
+  Object.defineProperty(ls, 'spaceRemained', {
+    get: function () {
+      return calculateAvailableSpace() - calculateSize();
+    }
+  });
+
+})(window);
